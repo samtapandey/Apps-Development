95f97f95e9fe3d88547ee755299a2f5108c66f2e
Sunakshi-Dev\Sunakshi <sunakshichagti@gmail.com>
2018-06-29 12:05:12 +0530

    Refresh and onload issues

--- scripts/app.js
+++ scripts/app.js
@@ -19,9 +19,6 @@ var ExportCSVApp = angular.module('ExportCSVApp',['ui.bootstrap',
         $routeProvider.when('/', {
             templateUrl:'components/home.html',
             controller: 'homeController'
-        }).when('/report',{
-            templateUrl:'components/report.html',
-            controller: 'reportController'
         }).otherwise({
             redirectTo : '/'
         });
