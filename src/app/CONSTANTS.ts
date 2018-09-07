920717c626b40b879f139e1716b3707daf6acca4
wasib <wasibtariq@gmail.com>
2018-09-07 10:36:46 +0530

    code updated with changes in the app

--- src/app/CONSTANTS.ts
+++ src/app/CONSTANTS.ts
@@ -6,17 +6,18 @@ export const attributes = [
     { id: 1, name: "id" },
     { id: 2, name: "code" },
     { id: 3, name: "name" },
-    { id: 4, name: "created" },
-    { id: 5, name: "lastUpdated" },
-    { id: 6, name: "formName" },
-    { id: 7, name: "valueType" },
-    { id: 8, name: "domainType" },
-    { id: 9, name: "publicAccess" }
+    { id: 4, name: "shortName" },
+    { id: 5, name: "created" },
+    { id: 6, name: "lastUpdated" },
+    { id: 7, name: "formName" },
+    { id: 8, name: "valueType" },
+    { id: 9, name: "domainType" },
+    { id: 10, name: "publicAccess" }
 ];
 export const groups = [
     { id: 1, name: selection },
-    { id: 2, name: "Data Element Groups" },
-    { id: 3, name: "Data Element" },
+    { id: 2, name: "Data Elements Groups" },
+    { id: 3, name: "Data Elements" },
     { id: 4, name: "Indicators Groups" },
     { id: 5, name: "Indicators" },
     { id: 6, name: "Organisation Unit groups" },
--- src/app/metadatareport/metadatareport.component.ts
+++ src/app/metadatareport/metadatareport.component.ts
@@ -43,6 +43,10 @@ export class MetadatareportComponent implements OnInit {
   public btnDisable: boolean = true;
   public btnDisable2: boolean = true;
   public butDisabled: boolean = false;
+  public excelbutton: boolean = false;
+  public theading: boolean = false;
+  public expanded: boolean = false;
+  public expandDisable: boolean = false;
   public selectedAttribute;
 
   selectedGroup = "Please Select";
@@ -50,69 +54,107 @@ export class MetadatareportComponent implements OnInit {
   public selectedMetaDataGroup = "";
   public groupObject = "";
   public allFields = [];
+  public allPush = [];
+  public allData = [];
   public responseData;
   public respData;
+  public pathData;
+  public hierarchy;
+  public val;
 
   constructor(private _customService: CustomService, private http: HttpClient) { }
 
   ngOnInit() {
-
+    setTimeout(() => {
+      var loader1 = document.getElementById("loader");
+      loader1.setAttribute('style', 'visibility:hidden !important');
+    }, 1000);
+    this.http.get("../../organisationUnits.json?fields=id,name&paging=false").subscribe((res: any[]) => {
+      console.log(res);
+      this.pathData = res;
+    });
   }
 
   public getReport(groupName, attributeName) {
+    this.allPush = []; this.allFields = [];
+    this.theading = false; this.excelbutton = false;
+
     console.log(groupName);
+    setTimeout(() => {
+      var loader1 = document.getElementById("loader");
+      loader1.setAttribute('style', 'visibility:visible !important');
+    }, 1000);
 
     if (attributeName == "All") {
       this.allFields = [];
-      this.allFields.push("id", "code", "name", "created", "lastUpdated", "formName", "valueType", "domainType", "publicAccess");
+      this.allFields.push("id", "code", "name", "shortName", "created", "lastUpdated", "formName", "valueType", "domainType", "publicAccess");
     }
     else {
       this.allFields = attributeName;
     }
     console.log(this.allFields);
 
-    if (groupName == "Data Element Groups") {
+    if (groupName == "Data Elements Groups") {
       // getDataElementGroup();
       this.groupObject = "dataElementGroups";
       console.log("dataElementGroups selected");
+      this.expandDisable = true;
     }
-    else if (groupName == "Data Element") {
+    else if (groupName == "Data Elements") {
       // getDataElement();
       this.groupObject = "dataElements";
+      this.expandDisable = false;
       console.log("dataElements selected");
     }
     else if (groupName == "Indicators Groups") {
       // getIndicatorGroups();
       this.groupObject = "indicatorGroups";
-      console.log("indicatorGroups selected")
+      this.expandDisable = true;
+      console.log("indicatorGroups selected");
     }
     else if (groupName == "Indicators") {
       // getIndicators();
       this.groupObject = "indicators";
+      this.expandDisable = false;
       console.log("indicators selected");
     }
     else if (groupName == "Organisation Units") {
       // getorganisationUnits();
       this.groupObject = "organisationUnits";
-      console.log("organisationUnits selected")
+      this.allFields.push("path");
+      this.expandDisable = false;
+      console.log("organisationUnits selected");
     }
     else if (groupName == "Organisation Unit groups") {
       // getOrgGroup();
       this.groupObject = "organisationUnitGroups";
-      console.log("organisationUnitGroups selected")
+      this.expandDisable = true;
+      console.log("organisationUnitGroups selected");
     }
     else if (groupName == "Users Groups") {
       // getUserGroup();
       this.groupObject = "userGroups";
-      console.log("userGroups selected")
+      this.expandDisable = true;
+      console.log("userGroups selected");
     }
     else if (groupName == "Users") {
       // getUsers();
       this.groupObject = "users";
+      this.expandDisable = false;
       console.log("users selected");
     }
     this.getApi();
+    return groupName;
+  }
 
+  Expand(expand) {
+    if (expand == false) {
+      this.expanded = true;
+    }
+    else {
+      this.expanded = false;
+    }
+    return this.expanded;
   }
 
   filterForeCasts(value) {
@@ -143,6 +185,7 @@ export class MetadatareportComponent implements OnInit {
   }
 
   getApi() {
+    this.respData = undefined;
     this.http.get("../../" + this.groupObject + ".json?fields=[" + this.allFields + "]&paging=false").subscribe((res: any[]) => {
       console.log(res);
       this.respData = res;
@@ -152,59 +195,84 @@ export class MetadatareportComponent implements OnInit {
     }, 1000);
   }
 
+  exportExcel(selectedGroup) {
+    var a = document.createElement('a');
+    var data_type = 'data:application/vnd.ms-excel';
+    var table_div = document.getElementById('reporttable');
+    var table_html = table_div.outerHTML.replace(/ /g, '%20');
+    a.href = data_type + ', ' + table_html;
+    a.download = selectedGroup + '.xls';
+    a.click();
+  }
+
+  printFunc() {
+    var divToPrint = document.getElementById("reporttable");
+    var newWin = window.open("");
+    newWin.document.write(divToPrint.outerHTML);
+    newWin.print();
+    newWin.close();
+  }
+
   getData() {
 
-    // this._customService.getAPI(groupObject, allFields)
-    // .subscribe(data => {this.respData = data});
-    // console.log(this.respData);
-
-    // CREATE DYNAMIC TABLE.
-    var table = document.createElement("table");
-    table.setAttribute("id", "myId");
-    table.setAttribute( "style", "border: 2px solid black;border-collapse: collapse;background-color: #D6EAF8;");
-
-    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-    var tr = table.insertRow(-1);        // TABLE ROW.
-    tr.setAttribute( "style", "border: 2px solid black;");
-
-    for (var i = 0; i < this.allFields.length; i++) {
-      var th = document.createElement("th");
-      th.setAttribute( "style", "border: 2px solid black;text-align: center;");
-      th.innerHTML = this.allFields[i];
-      tr.appendChild(th);
+    console.log(this.respData + "-" + this.groupObject);
+    if (this.respData == undefined || this.respData == null || this.respData == "") {
+      this.getApi();
     }
+    else {
+      var dataRows = this.respData[this.groupObject];
 
-    // ADD JSON DATA TO THE TABLE AS ROWS.
-    for (var i = 0; i < this.respData[this.groupObject].length; i++) {
-
-      tr = table.insertRow(-1);
-      tr.setAttribute( "style", "border: 1px solid black;");
-
-      for (var j = 0; j < this.allFields.length; j++) {
-        var tabCell = tr.insertCell(-1);
-        tabCell.setAttribute( "style", "border: 1px solid black;");
-        if (this.respData[this.groupObject][i][this.allFields[j]] == undefined || this.respData[this.groupObject][i][this.allFields[j]] == null || this.respData[this.groupObject][i][this.allFields[j]] == "") {
-          tabCell.innerHTML = "--";
-        }
-        else {
-          if (this.allFields[j] == "lastUpdated" || this.allFields[j] == "created") {
-            var dateValue1 = this.respData[this.groupObject][i][this.allFields[j]];
-            var dateValue2 = dateValue1.split("T");
-            var dateValue = dateValue2[0];
-            tabCell.innerHTML = dateValue;
+      for (var i = 0; i < dataRows.length; i++) {
+        for (var j = 0; j < this.allFields.length; j++) {
+          if (dataRows[i][this.allFields[j]] == undefined || dataRows[i][this.allFields[j]] == null || dataRows[i][this.allFields[j]] == "") {
+            this.val = "--";
           }
           else {
-            tabCell.innerHTML = this.respData[this.groupObject][i][this.allFields[j]];
+            if (this.allFields[j] == "lastUpdated" || this.allFields[j] == "created") {
+              var dateValue1 = dataRows[i][this.allFields[j]];
+              var dateValue2 = dateValue1.split("T");
+              var dateValue = dateValue2[0];
+              this.val = dateValue;
+            }
+            else if (this.allFields[j] == "path") {
+              var pathValue1 = dataRows[i][this.allFields[j]];
+              var pathValue2 = pathValue1.split("/");
+              var pathValue = this.getPath(pathValue2);
+              this.val = pathValue;
+            }
+            else {
+              this.val = dataRows[i][this.allFields[j]];
+            }
           }
+          //this.allData.push({ [this.allFields[j]]: this.val });
+          this.allData.push(this.val);
+        }
+        this.allPush.push(this.allData);
+        this.allData = [];
+      }
+    }
+    console.log(this.allPush);
+    setTimeout(() => {
+      var loader1 = document.getElementById("loader");
+      loader1.setAttribute('style', 'visibility:hidden !important');
+      this.excelbutton = true;
+      this.theading = true;
+    }, 1000);
+  }
+
+  getPath(pathId) {
+    let pathMap = [];
+    this.hierarchy = "";
+    for (let y = 0; y < pathId.length; y++) {
+      for (let z = 0; z < this.pathData.organisationUnits.length; z++) {
+        if (this.pathData.organisationUnits[z].id == pathId[y]) {
+          pathMap.push(this.pathData.organisationUnits[z].name);
         }
       }
     }
-
-    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-    var divContainer = document.getElementById("reporttable");
-    divContainer.innerHTML = "";
-    divContainer.appendChild(table);
+    for (let i = pathMap.length - 1; i >= 0; i--) {
+      this.hierarchy += pathMap[i] + "/";
+    }
+    return this.hierarchy;
   }
-
 }
