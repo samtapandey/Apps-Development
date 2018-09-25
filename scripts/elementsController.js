cd93753707133880a13ad22bfab3eff7c3df1a97
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-09-25 17:31:47 +0530

    Adding logics for passing and mapping programs and stages

--- scripts/elementsController.js
+++ scripts/elementsController.js
@@ -30,40 +30,86 @@ trackerElementsReport
     //onload functions
     window.onclick = function (event) {
       var modal = document.getElementById("myModal");
-      var span = document.getElementsByClassName("close")[0];
+      var span = document.getElementsByClassName("closeit")[0];
       if (event.target == span) {
         modal.style.display = "none";
       }
+
+      $(".selected").addClass("fa-check");
     }
-    $(document).on('keyup', function (evt) {
-      if (evt.keyCode == 27) {
-        var modal = document.getElementById("myModal");
-        modal.style.display = "none";
-      }
-      if (evt.keyCode == 13) {
-        execute();
-      }
-    });
 
     //initially load tree
     selection.load();
 
+    getAllPrograms();
+    getAllProgramStages();
     // Listen for OU changes
     selection.setListenerFunction(function () {
       $scope.selectedOrgUnitUid = selection.getSelected();
-      loadPrograms();
+      loadOus();
     }, false);
 
-    loadPrograms = function () {
+    loadOus = function () {
       MetadataService.getOrgUnit($scope.selectedOrgUnitUid).then(function (orgUnit) {
         $timeout(function () {
           $scope.selectedOrgUnit = orgUnit;
           if ($scope.popup == 'ou') {
-            $('#selectedItem').text($scope.selectedOrgUnit.name);
+            $scope.updateSelections("ou");
           }
         });
       });
     }
+
+    // $scope.selectedProgram = {};
+    function getAllPrograms() {
+      MetadataService.getAllPrograms().then(function (prog) {
+        $scope.allPrograms = prog.programs;
+        $scope.programs = [];
+        for (var i = 0; i < prog.programs.length; i++) {
+          //if (prog.programs[i].withoutRegistration == false) {
+          $scope.programs.push(prog.programs[i]);
+        }
+      });
+    }
+
+    function getAllProgramStages() {
+      MetadataService.getProgramStages().then(function (stages) {
+        $scope.programStages = [];
+        for (var j = 0; j < stages.programStages.length; j++) {
+          $scope.programStages.push(stages.programStages[j]);
+        }
+      });
+    };
+
+
+    //function to change selections in popup
+
+    $scope.updateSelections = function (value) {
+      switch (value) {
+        case "ou":
+          $('#selectedItem').text($scope.selectedOrgUnit.name);
+          break;
+
+        case "pr":
+          var totalPrograms = Object.keys($scope.selectedPrograms).length;
+          if (totalPrograms != 0) {
+            var firstProgram = $scope.selectedPrograms[Object.keys($scope.selectedPrograms)[0]].name;
+            (totalPrograms == 1) ? $('#selectedItem').text(firstProgram): $('#selectedItem').text(firstProgram + ", +" + (totalPrograms - 1));
+          } else {
+            $('#selectedItem').text("");
+          }
+          break;
+
+        case "ps":
+          break;
+
+        case "de":
+          break;
+
+        default:
+          break;
+      }
+    };
 
     // function to view popups
     $scope.updatePopup = function (v) {
@@ -74,16 +120,20 @@ trackerElementsReport
       }
       if (v == 'pr') {
         $scope.popup = 'pr';
-        $('#selectedItem').text(($scope.selectedPrograms[0] === undefined ? "" : $scope.selectedPrograms[0] + ", +") + $scope.selectedPrograms.length == 0 ? "" : $scope.selectedPrograms.length);
+        var totalPrograms = Object.keys($scope.selectedPrograms).length;
+        if (totalPrograms != 0) {
+          var firstProgram = $scope.selectedPrograms[Object.keys($scope.selectedPrograms)[0]].name;
+          (totalPrograms == 1) ? $('#selectedItem').text(firstProgram): $('#selectedItem').text(firstProgram + ", +" + (totalPrograms - 1));
+        } else {
+          $('#selectedItem').text("");
+        }
       }
       if (v == 'ps') {
         $scope.popup = 'ps';
-        // $('#selectedItem').text($scope.selectedPs[0] + ", +" + $scope.selectedPs.length);
         $('#selectedItem').text(($scope.selectedPs[0] === undefined ? "" : $scope.selectedPs[0] + ", +") + $scope.selectedPs.length == 0 ? "" : $scope.selectedPs.length);
       }
       if (v == 'de') {
         $scope.popup = 'de';
-        // $('#selectedItem').text($scope.selectedDes[0] + ", +" + $scope.selectedDes.length);
         $('#selectedItem').text(($scope.selectedDes[0] === undefined ? "" : $scope.selectedDes[0] + ", +") + $scope.selectedDes.length == 0 ? "" : $scope.selectedDes.length);
       }
       document.getElementById('loader').style.display = "block";
@@ -96,19 +146,58 @@ trackerElementsReport
 
     $scope.params = function (v) {
 
-      if ($scope.popup == 'ou') {
-        $('#ouBtn').html("<i class='fa fa-plus'></i> " + $scope.selectedOrgUnit.name);
-        $("#ouBtn").addClass("btn-success");
-      }
-      if ($scope.popup == 'pr') {
+      switch ($scope.popup) {
 
-      }
-      if ($scope.popup == 'ps') {
+        case "ou":
+          $('#ouBtn').html("<i class='fa fa-plus'></i> " + $scope.selectedOrgUnit === undefined ? "" : $scope.selectedOrgUnit.name);
+          $("#ouBtn").addClass("btn-success");
+          break;
 
-      }
-      if ($scope.popup == 'de') {
+        case "pr":
+          var totalPrograms = Object.keys($scope.selectedPrograms).length;
+          if (totalPrograms != 0) {
+            var firstProgram = $scope.selectedPrograms[Object.keys($scope.selectedPrograms)[0]].name;
+            (totalPrograms == 1) ? $('#prBtn').html("<i class='fa fa-plus'></i> " + firstProgram): $('#prBtn').html("<i class='fa fa-plus'></i> " + (firstProgram + ", +" + (totalPrograms - 1)));
+            $("#prBtn").addClass("btn-success");
+            $scope.updateProgramStages($scope.selectedPrograms);
+          }
+          break;
 
+        case "ps":
+          break;
+
+        case "de":
+          break;
+
+        default:
+          return;
       }
       document.getElementById('myModal').style.display = 'none';
     }
+
+    $scope.programSelections = function (index, pr) {
+      var $this = $('.pr-table tr').eq(index).children('td:first').find('i');
+      if (!$this.hasClass('selected')) {
+        $scope.selectedPrograms[pr.id] = pr;
+        $this.addClass('selected fa-check');
+      } else {
+        delete $scope.selectedPrograms[pr.id];
+        $this.removeClass('selected fa-check');
+      }
+      $scope.updateSelections("pr");
+    };
+
+
+    $scope.updateProgramStages = function (arr) {
+      $scope.mappedProgramStages = [];
+      for (var k = 0; k < Object.keys(arr).length; k++) {
+        $scope.mappedProgramStages.push(arr[Object.keys(arr)[k]].name);
+        for (var i = 0; i < $scope.programStages.length; i++) {
+          // console.log(Object.keys(arr)[k] + "---"  + $scope.programStages[i].program.name);
+          if (Object.keys(arr)[k] == $scope.programStages[i].program.id) {
+            $scope.mappedProgramStages.push($scope.programStages[i]);
+          }
+        }
+      }
+    };
   });
