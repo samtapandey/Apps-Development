b3b111205ecbdcfe55fcfb43a8d2df37651febf9
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-10-04 17:36:57 +0530

    filteration on basis of registration program or without

--- scripts/elementsController.js
+++ scripts/elementsController.js
@@ -17,7 +17,7 @@ trackerElementsReport.directive('calendar', function () {
   };
 });
 trackerElementsReport
-  .controller('elementsController', function ($rootScope,
+  .controller('elementsController', function ($window,
     $scope,
     $timeout,
     MetadataService) {
@@ -53,7 +53,6 @@ trackerElementsReport
       }, function (start, end, label) {
         $scope.startdate = start.format('YYYY-MM-DD');
         $scope.enddate = end.format('YYYY-MM-DD');
-        $scope.loadMetaData();
       });
 
     });
@@ -113,14 +112,14 @@ trackerElementsReport
       // });
       // saveAs(blob, "Tracker Rate Report.xls");
 
-        //getting data from our table
-        var data_type = 'data:application/vnd.ms-excel';
-        var table_div = id;
-        var table_html = table_div.outerHTML.replace(/ /g, '%20');
-        var a = document.createElement('a');
-        a.href = data_type + ', ' + table_html;
-        a.download = 'Tracker Rate Report.xls';
-        a.click();
+      //getting data from our table
+      var data_type = 'data:application/vnd.ms-excel';
+      var table_div = id;
+      var table_html = table_div.outerHTML.replace(/ /g, '%20');
+      var a = document.createElement('a');
+      a.href = data_type + ', ' + table_html;
+      a.download = 'Tracker Rate Report.xls';
+      a.click();
 
     };
 
@@ -373,8 +372,12 @@ trackerElementsReport
       $scope.selectalldes = !$scope.selectalldes;
     }
 
-    $scope.generateReport = function () {
+    $scope.generateReport = function (type) {
+      if (type == "ev") $scope.loadMetaDataEv();
+      else $scope.loadMetaDataEn();
+    };
 
+    $scope.generateReportByEvents = function () {
       $('.data-table').empty();
       for (var i = 0; i < Object.keys($scope.selectedPrograms).length; i++) {
         for (var j = 0; j < Object.keys($scope.selectedPrograms[Object.keys($scope.selectedPrograms)[i]].selectedPS).length; j++) {
@@ -408,15 +411,50 @@ trackerElementsReport
       }
     };
 
+    $scope.generateReportByEnrollments = function () {
+      $('.data-table').empty();
+      for (var i = 0; i < Object.keys($scope.selectedPrograms).length; i++) {
+        if (!$scope.selectedPrograms[Object.keys($scope.selectedPrograms)[i]].withoutRegistration) {
+          for (var j = 0; j < Object.keys($scope.selectedPrograms[Object.keys($scope.selectedPrograms)[i]].selectedPS).length; j++) {
+            var prid = $scope.selectedPrograms[Object.keys($scope.selectedPrograms)[i]].id;
 
-    $scope.loadMetaData = function () {
+            var selectedPsId = Object.keys($scope.selectedPrograms[Object.keys($scope.selectedPrograms)[i]].selectedPS)[j];
+
+            var table = "<div id='div_" + selectedPsId + "' class='container'><table class='table table-hovered table-bordered table-striped custom-data-table' id='table_" + selectedPsId + "'>";
+
+            var programHeaderRow = "<thead><tr><th colspan='3'>Program:  " + $scope.selectedPrograms[Object.keys($scope.selectedPrograms)[i]].name + "<span class='icon-div' onClick='fnExcelReport(table_" + selectedPsId + ")'><i class='fa fa-download icon-table' data-toggle='tooltip' data-placement='top' title='Export this table in excel'></i></span><span class='icon-div'><i class='fa fa-bar-chart icon-table' data-toggle='tooltip' data-placement='top' title='Switch to graph'></i></span></th></tr>";
+
+            var programStageHeaderRow = "<tr><th colspan='3'>Program Stage:  " + $scope.selectedPrograms[Object.keys($scope.selectedPrograms)[i]].selectedPS[selectedPsId] + "<p style='float:right;color:white'>Total Enrollments : " + $scope.elementsMap[selectedPsId] + "</p></th></tr>";
+
+            var deHeaderRow = "<tr><th>Data Elements</th><th>Count</th><th>Percentage (%)</th></tr></thead>"
+            var selectedDeRow = "";
+            for (var k = 0; k < Object.keys($scope.selectedDes).length; k++) {
+              var DePsId = $scope.selectedDes[Object.keys($scope.selectedDes)[k]].psid;
+              var value = "";
+              ($scope.elementsMap[selectedPsId + "_" + deid] === undefined) ? value = 0: value = $scope.elementsMap[selectedPsId + "_" + deid];
+              if (selectedPsId == DePsId) {
+                var deid = $scope.selectedDes[Object.keys($scope.selectedDes)[k]].deid;
+                selectedDeRow = selectedDeRow + "<tr><td>" + $scope.selectedDes[Object.keys($scope.selectedDes)[k]].dename + "</td><td id='" + deid + "'>" +
+                  value +
+                  "</td><td>" + ((value / $scope.elementsMap[selectedPsId]) * 100).toFixed(2) + "</td></tr>";
+              }
+            }
+            var tableAppend = table + programHeaderRow + programStageHeaderRow + deHeaderRow + selectedDeRow + "</table></div><br><br>";
+            $(".data-table").append(tableAppend);
+          }
+        }
+      }
+    };
+
+
+    $scope.loadMetaDataEv = function () {
       $scope.elementsMap = [];
       document.getElementById('loader2').style.display = "block";
       document.getElementById("loadtext").innerHTML = "loading metadata.."
       MetadataService.getEvents($scope.selectedOrgUnit.id, $scope.startdate, $scope.enddate).then(function (data) {
         var events = data.events;
         for (var p = 0; p < events.length; p++) {
-          var perc = ((p / events.length - 1) / 100).toFixed(2);
+          var perc = ((p / events.length - 1) * 100).toFixed(2);
           document.getElementById("loadtext").innerHTML = perc + "% metadata loaded!";
           if (events[p].eventDate !== undefined) {
             var date = events[p].eventDate;
@@ -424,7 +462,7 @@ trackerElementsReport
             var evdate = new Date(first);
             if (evdate <= new Date($scope.enddate) && evdate >= new Date($scope.startdate)) {
               var ps = events[p].programStage;
-              $scope.generateEventsMap(ps);
+              $scope.generateElementsMap(ps);
               for (var q = 0; q < events[p].dataValues.length; q++) {
                 var de = events[p].dataValues[q].dataElement;
                 $scope.generateElementsMap(ps, de);
@@ -433,26 +471,34 @@ trackerElementsReport
           }
           if (p == events.length - 1) {
             document.getElementById('loader2').style.display = "none";
+            $scope.generateReportByEvents();
           }
         }
       });
     };
 
     $scope.generateElementsMap = function (ps, de) {
-      if ($scope.elementsMap[ps + "_" + de] === undefined) {
-        $scope.elementsMap[ps + "_" + de] = 1;
+
+      if (de === undefined) {
+        if ($scope.elementsMap[ps] === undefined) {
+          $scope.elementsMap[ps] = 1;
+        } else {
+          $scope.elementsMap[ps] += 1;
+        }
       } else {
-        $scope.elementsMap[ps + "_" + de] += 1;
+        if ($scope.elementsMap[ps + "_" + de] === undefined) {
+          $scope.elementsMap[ps + "_" + de] = 1;
+        } else {
+          $scope.elementsMap[ps + "_" + de] += 1;
+        }
       }
     };
 
-    $scope.generateEventsMap = function (ps) {
-      if ($scope.elementsMap[ps] === undefined) {
-        $scope.elementsMap[ps] = 1;
-      } else {
-        $scope.elementsMap[ps] += 1;
-      }
-    };
+    $scope.home = function () {
+      var host = $window.location.href;
+      var newLocation = host.split('api')[0] + "dhis-web-dashboard-integration/index.html";
+      $window.location.href = newLocation;
+    }
 
 
   });
