52860d0d111586e8ec4c708c5425da0693f9f506
sudiksha1989 <snagvanshi06@gmail.com>
2018-12-06 14:58:10 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,28 +1,44 @@
-# Apps-Development
-HISP INDIA External Apps Development
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Available Scripts
 
- # Link of other external Apps developed:
+In the project directory, you can run:
 
- 1# Tracker data approval app developed for UPHMIS
-    https://github.com/hispindia/dhis-tracker-data-approval-app
+### `npm start`
 
- 2# e-Aushadhi Stock Data Import App developed for malaria
-    https://github.com/hispindia/dhis-app-excel-import/tree/Aggrigate-Data-Excel-Import-App-Without-Mapping
-	
- 3# Aggregate data excel import app developed for maharashtra with multiple OU and multiple Data Element
-    https://github.com/hispindia/dhis-app-excel-import/tree/Aggregate-Data-Excel-Import-App-MultipleOU-MultipleDE
- 
- 4# Excel import app for multiple stages version 2.27
-    https://github.com/hispindia/dhis-app-excel-import/tree/2.27-MWMIS-Odisha-Multistage-Excel-Import 
-	
- 5# Excel import app for multiple stages version 2.26
-    https://github.com/hispindia/dhis-app-excel-import/tree/2.26-AES-Multistage-Excel-Import
-	
- 6# Excel import app for aggrigate & tracker data version 2.27
-    https://github.com/hispindia/dhis-app-excel-import/tree/csv-2.27
-    
- 7# Tracker and event data exporter app ( generic, msf, tibet branches )  
-    https://github.com/hispindia/tracker-data-exporter-app/
-	
-	
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br>
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
---
+++ src/components/DataElements/index.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import { FormGroup, ControlLabel, FormControl, Button, HelpBlock } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { getDataElementValues } from '../actions/serviceActions';
+import { handleChange, handleInputChange } from "./updateDataElements";
+import { handleSubmit } from "./validate"
+import { postDataElements } from '../actions/serviceActions';
+import { updateFormInput, validateFormInput } from '../actions/dataElementAction';
+
+class DataElements extends React.Component {
+
+    componentDidMount() {
+        this.props.getDataElementValues()
+    }
+    render() {
+
+        var formdata = []
+        for (var i = 0; i <= this.props.dataElement.formNameCreated.length; i++) {
+            for (var key in this.props.dataElement.formNameCreated[i])
+                formdata.push(<tr><td>{key}</td><td>{this.props.dataElement.formNameCreated[i][key]}</td></tr>)
+        }
+
+
+        return (
+            <div className="display-form">
+                <div className="header"><span className="headertext">CREATE DATA-ELEMENTS</span></div>
+                <div className="body-content">
+                    <div className="left-view">
+                        <form onSubmit={event => handleSubmit(this, event)}>
+                            <FormGroup controlId="formTEI" validationState={this.props.dataElement.attrValueValidy}>
+                                <ControlLabel>ANTIBIOTICS</ControlLabel>
+                                <FormControl componentClass="select" onChange={event => handleChange(this, event, 'UPDATE-TEI')} multiple>
+                                    {this.props.dataElement.attrValue.map((arr) => <option value={arr.value}>{arr.name}</option>)}
+                                </FormControl>
+                                {(this.props.dataElement.attrValueValidy === 'error') ? <HelpBlock>No antibiotic selected.</HelpBlock> : false}
+                            </FormGroup>
+
+                            <FormGroup controlId="formOptnSettTest" validationState={this.props.dataElement.testTypeValidy}>
+                                <ControlLabel>TEST TYPE</ControlLabel>
+                                <FormControl componentClass="select" onChange={event => handleChange(this, event, 'UPDATE-TESTTYPE')} multiple>
+                                    {this.props.dataElement.testType.map((arr) => <option value={arr.value}>{arr.name}</option>)}
+                                </FormControl>
+                                {(this.props.dataElement.testTypeValidy === 'error') ? <HelpBlock>No test type selected.</HelpBlock> : false}
+                            </FormGroup>
+
+                            <FormGroup controlId="formQueueSet" validationState={this.props.dataElement.guidelineValidy}>
+                                <ControlLabel>GUIDELINE</ControlLabel>
+                                <FormControl componentClass="select" onChange={event => handleChange(this, event, 'UPDATE-GUIDELINE')} multiple>
+                                    {this.props.dataElement.guideline.map((arr) => <option value={arr.value}>{arr.name}</option>)}
+                                </FormControl>
+                                {(this.props.dataElement.guidelineValidy == 'error') ? <HelpBlock>No guideline selected.</HelpBlock> : false}
+                            </FormGroup>
+
+                            <FormGroup controlId="formConcen" validationState={this.props.dataElement.inputBoxValidy}>
+                                <ControlLabel>CONCENTRATION</ControlLabel>
+                                <FormControl type="text" placeholder="Enter number" value={this.props.dataElement.inputConcentation} onChange={event => handleInputChange(this, event, 'UPDATE-CONCENTRATION')} disabled={false} />
+                                {(this.props.dataElement.inputConcentation === '') ?
+                                    <HelpBlock>No value entered.</HelpBlock> : false
+                                }
+                            </FormGroup>
+
+                            <FormGroup controlId="formSampleSource" validationState={this.props.dataElement.sampleSourceValidy}>
+                                <ControlLabel>SAMPLE SOURCE</ControlLabel>
+                                <FormControl componentClass="select" onChange={event => handleChange(this, event, 'UPDATE-SOURCETYPE')} multiple>
+                                    {this.props.dataElement.sampleSource.map((arr) => <option value={arr.value}>{arr.name}</option>)}
+                                </FormControl>
+                                {(this.props.dataElement.sampleSourceValidy === 'error') ? <HelpBlock>No sample source selected.</HelpBlock> : false}
+                            </FormGroup>
+                            <Button className="button" type="submit">Create</Button>
+                        </form>
+                    </div>
+                    <div className="right-view">
+                        <div className="responsetable">
+                            <table id="displayDE">
+                                <tr>
+                                    <th>DATA ELEMENTS</th>
+                                    <th>RESPONSE</th>
+                                </tr>
+                                {formdata}
+                                
+                        </table>
+                    </div>
+                </div>
+            </div></div >
+
+        )
+
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        dataElement: state.DataElement,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getDataElementValues: () => {
+            dispatch(getDataElementValues())
+        },
+        updateFormInput: (param, selectedOption) => {
+            dispatch(updateFormInput(param, selectedOption))
+        },
+        postDataElements: (data) => {
+            dispatch(postDataElements(data))
+        },
+        validateFormInput: (formTEIVal, formOptnSettTestVal, formQueueSetVal, formSampleSourceVal, formConcenVal) => {
+            dispatch(validateFormInput(formTEIVal, formOptnSettTestVal, formQueueSetVal, formSampleSourceVal, formConcenVal))
+        },
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(DataElements)
